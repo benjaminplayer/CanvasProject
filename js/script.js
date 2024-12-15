88f1996b4f22dc3ea57bf3d972ca90d4b39a2e04
Benjamin <fireblazegaming21@gmail.com>
2024-12-15 02:21:57 +0100

    Some small stylish changes
    Styled sidebar and the login button.
    Added options button
    Still have to make sidebar scrollable

--- js/script.js
+++ js/script.js
@@ -196,3 +196,17 @@ function openMenu(button){
     button.nextElementSibling.classList.toggle('show');
 }
 
+const element = document.getElementById('sidebar');
+
+const observer = new ResizeObserver(() => {
+    const isOverflowing = element.scrollWidth > element.clientWidth || element.scrollHeight > element.clientHeight;
+    if (isOverflowing) {
+        console.log('Element is overflowing!');
+        element.setAttribute('overflow-y','scroll')
+    } else {
+        console.log('Element is not overflowing.');
+        element.removeAttribute('overflow-y')
+    }
+});
+
+observer.observe(element);
